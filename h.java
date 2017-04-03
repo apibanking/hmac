1d94b2f55132ff2e463435907e8101c30074e8c0
akil <kataria.subs@gmail.com>
2017-04-03 15:06:48 +0530

    one function

--- h.java
+++ h.java
@@ -10,29 +10,20 @@ class H {
 
   public static void main(String[] args) {
     String req = "{\"amount\":10000,\"txnRefId\":\"P16111764473891\",\"accountNumber\":\"SHOPXDT051624\"}";
-    System.out.println("body " + req + " hamc " + calculateHMAC("2a5f1f65145c4d5daad65c902c19b021", calculateMD5(req)));
+    System.out.println("body " + req + " hamc " + calculateHMAC("2a5f1f65145c4d5daad65c902c19b021", req));
   }
-
-	private static String calculateMD5(String contentToEncode) {
-           try {
-		MessageDigest digest = MessageDigest.getInstance("MD5");
-		digest.update(contentToEncode.getBytes());
-		String result = new String(DatatypeConverter.printBase64Binary(digest.digest()));
-                System.out.println(result);
-		return result;
-           } catch (NoSuchAlgorithmException e) {
-                return null;
-           }
-	}
 
         private static String calculateHMAC(String secret, String data) {
 		try {
+		        MessageDigest digest = MessageDigest.getInstance("MD5");
+        		digest.update(data.getBytes());
+        		String result = new String(DatatypeConverter.printBase64Binary(digest.digest()));
+
 			SecretKeySpec signingKey = new SecretKeySpec(secret.getBytes(),	HMAC_SHA1_ALGORITHM);
 			Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
 			mac.init(signingKey);
-			byte[] rawHmac = mac.doFinal(data.getBytes());
-			String result = new String(DatatypeConverter.printBase64Binary(rawHmac));
-                        System.out.println(result);
+			byte[] rawHmac = mac.doFinal(result.getBytes());
+			result = new String(DatatypeConverter.printBase64Binary(rawHmac));
 			return result;
 		} catch (GeneralSecurityException e) {
                         return null;
