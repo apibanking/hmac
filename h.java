766079d5a2e74295d329d02ff47eb991bbef0868
akil <kataria.subs@gmail.com>
2017-04-03 15:03:33 +0530

    with hmac

--- h.java
+++ h.java
@@ -9,15 +9,20 @@ class H {
   private final static String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 
   public static void main(String[] args) {
-    String req = "{ \"amount\": 10000, \"txnRefId\": \"P16111764973885\", \"accountNumber\": \"SHOPXDT051624\" }";
-    System.out.println("body " + req + " hamc " + calculateHMAC("2a5f1f65145c4d5daad65c902c19b021", req));
+    String req = "{\"amount\":10000,\"txnRefId\":\"P16111764473891\",\"accountNumber\":\"SHOPXDT051624\"}";
+    System.out.println("body " + req + " hamc " + calculateHMAC("2a5f1f65145c4d5daad65c902c19b021", calculateMD5(req)));
   }
 
-	private static String calculateMD5(String contentToEncode) throws NoSuchAlgorithmException {
+	private static String calculateMD5(String contentToEncode) {
+           try {
 		MessageDigest digest = MessageDigest.getInstance("MD5");
 		digest.update(contentToEncode.getBytes());
-		String result = new String(DatatypeConverter.printHexBinary(digest.digest()));
+		String result = new String(DatatypeConverter.printBase64Binary(digest.digest()));
+                System.out.println(result);
 		return result;
+           } catch (NoSuchAlgorithmException e) {
+                return null;
+           }
 	}
 
         private static String calculateHMAC(String secret, String data) {
@@ -26,10 +31,11 @@ class H {
 			Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
 			mac.init(signingKey);
 			byte[] rawHmac = mac.doFinal(data.getBytes());
-			String result = new String(DatatypeConverter.printHexBinary(rawHmac));
+			String result = new String(DatatypeConverter.printBase64Binary(rawHmac));
+                        System.out.println(result);
 			return result;
 		} catch (GeneralSecurityException e) {
-			throw new IllegalArgumentException();
+                        return null;
 		}
 	}
 }
